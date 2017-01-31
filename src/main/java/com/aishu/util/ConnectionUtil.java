3a2bcab2ebbd93bd6617689a988510f5752aeb51
AiswaryaRavi <aiswaryaravindran240@gmail.com>
2017-01-31 14:39:04 +0530

    updated

--- src/main/java/com/aishu/util/ConnectionUtil.java
+++ src/main/java/com/aishu/util/ConnectionUtil.java
@@ -1,8 +1,6 @@
 package com.aishu.util;
 
-import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
+
 
 import javax.sql.DataSource;
 
@@ -13,21 +11,13 @@ public class ConnectionUtil {
 	private ConnectionUtil() {
 		throw new IllegalAccessError("Utility class");
 	}
+
 	public static DataSource getDataSource() {
 		BasicDataSource ds = new BasicDataSource();
 		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		ds.setUsername("dev_user");
 		ds.setPassword("dev_pwd");
-		String ip = "139.59.29.57";
-		try {
-			Socket socket = new Socket(ip, 3306);
-		} catch (UnknownHostException e) {
-			
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		ds.setUrl("jdbc:mysql://socket/aishwarya_db");
+		ds.setUrl("jdbc:mysql://139.59.27.59:3306/aishwarya_db");
 		return ds;
 	}
 
